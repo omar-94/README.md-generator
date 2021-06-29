e1310cd1f67ef42f73dcc2a87f137e348a1ab313
onava <enava23@yahoo.com>
2021-06-28 23:00:58 -0400

    inquier.prompt made into function

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const inquirer = require('inquirer');
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
-// Function to generatet README.md file
-
+// Function to generate README.md file
 const generateREADME = (answers) =>
 `# ${answers.title}
 
@@ -34,30 +33,30 @@ const generateREADME = (answers) =>
     ${answers.license}
 `
 
-inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project? \n',
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Please provide a description of your project. Things to consider: \n 1. What was your motivation? \n 2. Why did you build this project? \n 3. What problem does it solve? \n 4. What did you learn? \n'
-      },
-			{
-				type: 'input',
-				name: 'installation',
-				message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development enviroment running. \n'
-			},
-			{
-				type: 'input',
-				name: 'usage',
-				message: 'Provide instructions and examples for use. '
-			}
-    ])
-
+const promptUser = () => {
+	return inquirer.prompt([
+		{
+			type: 'input',
+			name: 'title',
+			message: 'What is the title of your project? \n',
+		},
+		{
+			type: 'input',
+			name: 'description',
+			message: 'Please provide a description of your project. Things to consider: \n 1. What was your motivation? \n 2. Why did you build this project? \n 3. What problem does it solve? \n 4. What did you learn? \n'
+		},
+		{
+			type: 'input',
+			name: 'installation',
+			message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development enviroment running. \n'
+		},
+		{
+			type: 'input',
+			name: 'usage',
+			message: 'Provide instructions and examples for use. '
+		}
+	]);
+}
 
 const init = () => {
 	promptUser()
